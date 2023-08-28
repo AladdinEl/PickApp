26ed0e9792962057ff22b755a38c840c2d05e326
Aladdin Elkhatib <elkhatibaladdin@gmail.com>
2023-08-28 10:20:44 +0200

    Add project files.

---
+++ KomOchHämta/Views/Account/RegisterVM.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KomOchHämta.Views.Account
+{
+	public class RegisterVM
+	{
+		[Required]
+		public string Username { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		[Display(Name = "Repeat password")]
+		[Compare(nameof(Password))]
+		public string PasswordRepeat { get; set; }
+	}
+}
