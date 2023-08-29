468cb30f4b59feaa9ab9b329d23c0ff2c9fba3c3
Fredrik Andersson <fredrik.andersson@brightstraining.com>
2023-08-29 09:11:19 +0200

    skapade indexVM samt Productscontroller

--- KomOchHämta/Views/Account/RegisterVM.cs
+++ KomOchHämta/Views/Account/RegisterVM.cs
@@ -8,13 +8,28 @@ namespace KomOchHämta.Views.Account
 		public string Username { get; set; }
 
 		[Required]
+		[Display(Name ="Lösenord")]
 		[DataType(DataType.Password)]
 		public string Password { get; set; }
 
 		[Required]
 		[DataType(DataType.Password)]
-		[Display(Name = "Repeat password")]
+		[Display(Name = "Upprepa lösenord")]
 		[Compare(nameof(Password))]
 		public string PasswordRepeat { get; set; }
-	}
+
+		[Required]
+		[Display(Name ="Förnamn")]
+        public string FirstName { get; set; }
+
+		[Required]
+		[Display(Name = "Efternamn")]
+		public string LastName { get; set; }
+
+
+		[Required]
+		[Display(Name = "E-mail")]
+		[EmailAddress(ErrorMessage = "Ogiltig e-mail adress")]
+		public string Email { get; set; }
+    }
 }
---
+++ KomOchHämta/Views/Products/IndexVM.cs
@@ -0,0 +1,10 @@
+﻿namespace KomOchHämta.Views.Products
+{
+	public class IndexVM
+	{
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Image { get; set; }
+
+    }
+}
